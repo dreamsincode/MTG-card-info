40265b59d4ca83db30222140588a17401938a687
dreamsincode <jaischmidt11@gmail.com>
2016-12-02 00:43:05 -0500

    Add files via upload

--- mtg.py
+++ mtg.py
@@ -6,10 +6,23 @@ import urllib
 page = urllib.urlopen("http://gatherer.wizards.com/Pages/Search/Default.aspx?color=|[W]|[U]|[B]|[R]|[G]")
 soup = BeautifulSoup(page)
 
-spans  = soup.find_all("span", {"class": "cardTitle"})
+cardtitle  = soup.find_all("span", {"class": "cardTitle"})
+type = soup.find_all("span", {"class": "typeLine"})
+mana = soup.find_all("span", {"class": "convertedManaCost"})
+deck = soup.find_all("img")
+extra = soup.find_all("div", {"class": "rightCol" } )
+soup.decompose(extra)
 
-for span in spans:
+for span in cardtitle:
     links = span.findAll('a')
     for a in links:
         print a.text
+    for span in type:
+        print span.text
+    for span in mana:
+        print span.text
+    for img in deck:
+        color = img.get('alt', '')
+        print color
+
 
