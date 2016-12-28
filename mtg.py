ca6720b8f479247884f6edc60f56ef23e58a694b
dreamsincode <jaischmidt11@gmail.com>
2016-12-27 23:31:50 -0500

    Update mtg.py

--- mtg.py
+++ mtg.py
@@ -1,31 +1,88 @@
 from bs4 import BeautifulSoup
-import csv
 import urllib
 import csv, os
-
-page = urllib.urlopen("http://gatherer.wizards.com/Pages/Search/Default.aspx?color=|[W]|[U]|[B]|[R]|[G]")
-soup = BeautifulSoup(page)
-
-cardtitle  = soup.find_all("span", {"class": "cardTitle"})
-t = soup.find_all("span", {"class": "typeLine"})
-m = soup.find_all("span", {"class": "convertedManaCost"})
-
-f = csv.writer(open("mtg.csv", "w"))
-f.writerow(["Name", "Type", "Mana"])
+import re
 
 
+class Gathering():
 
-for span in cardtitle:
-    links = span.findAll('a')
-    for a in links:
-        title = a.text.encode('UTF-8')
-for span in t:
-    type = span.text.encode('UTF-8')
-for span in m:
-    mana = span.text.encode('UTF-8')
+    def __init__(self):
+        self.decklist = [["Name", "Type", "Mana", "Deck"]]
 
-for rows in data:
-    f.writerow([title, type, mana])
+    def mainmethod(self):
+        for x in xrange(0, 142):
+            self.page = urllib.urlopen("http://gatherer.wizards.com/Pages/Search/Default.aspx?page={0}&color=|[W]|[U]|[B]|[R]|[G]".format(x))
+            self.soup = BeautifulSoup(self.page)
+            self.card_info()
+            print("Completed page: {0}".format(x))
+        self.csv_writer()
+
+    def card_info(self):
+        self.cardinfo = self.soup.find_all("div", attrs={"class": "cardInfo"})
+        for card in self.cardinfo:
+            self.card = card
+            self.title_parse()
+            self.type_parse()
+            self.converted_mana_parse()
+            self.mana_cost_parse()
+            self.decklist.append([self.cardtitle, self.type, self.mana])
+
+    def title_parse(self):
+        self.cardtitle = self.card.findNext("span", {"class": "cardTitle"}).text.strip().encode('utf-8')
+
+    def type_parse(self):
+        self.type = self.card.findNext("span", {"class": "typeLine"}).text.strip().encode('utf-8')
+
+    def converted_mana_parse(self):
+        self.conv_mana = self.card.findNext("span", {"class": "convertedManaCost"}).text.strip().encode('utf-8')
+
+    def mana_cost_parse(self):
+        self.mana_cost = self.card.findNext("span", {"class": "manaCost"})
+        for img in self.mana_cost.find_all("img"):
+            try:
+from bs4 import BeautifulSoup
+import urllib
+import csv, os
+import re
+
+
+class Gathering():
+
+    def __init__(self):
+        self.decklist = [["Name", "Type", "Mana", "Deck"]]
+
+    def mainmethod(self):
+        for x in xrange(0, 142):
+            self.page = urllib.urlopen("http://gatherer.wizards.com/Pages/Search/Default.aspx?page={0}&color=|[W]|[U]|[B]|[R]|[G]".format(x))
+            self.soup = BeautifulSoup(self.page)
+            self.card_info()
+            print("Completed page: {0}".format(x))
+        self.csv_writer()
+
+    def card_info(self):
+        self.cardinfo = self.soup.find_all("div", attrs={"class": "cardInfo"})
+        for card in self.cardinfo:
+            self.card = card
+            self.title_parse()
+            self.type_parse()
+            self.converted_mana_parse()
+            self.mana_cost_parse()
+            self.decklist.append([self.cardtitle, self.type, self.mana])
+
+    def title_parse(self):
+        self.cardtitle = self.card.findNext("span", {"class": "cardTitle"}).text.strip().encode('utf-8')
+
+    def type_parse(self):
+        self.type = self.card.findNext("span", {"class": "typeLine"}).text.strip().encode('utf-8')
+
+    def converted_mana_parse(self):
+        self.conv_mana = self.card.findNext("span", {"class": "convertedManaCost"}).text.strip().encode('utf-8')
+
+    def mana_cost_parse(self):
+        self.mana_cost = self.card.findNext("span", {"class": "manaCost"})
+        for img in self.mana_cost.find_all("img"):
+            try:
+
 
 
 
